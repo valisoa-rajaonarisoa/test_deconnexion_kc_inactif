1c4b9ff9b4dd624d287ad349ea5bd28a2f277675
valisoa-rajaonarisoa <valisoarajaonarisoa27@gmail.com>
2024-12-03 21:00:11 +0300

    la page login sans logique

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='login'>
+
+      <h2 className='mx-auto text-zinc-300 text-3xl'>Let's go ! </h2>
+      <div class="inputBox">
+        <input type="text" required="required"/>
+        <span>email</span>
+      </div>
+
+      <div class="inputBox">
+        <input type="text" required="required"/>
+        <span>password</span>
+      </div>
+
+      <button className="text-zinc-300 bg-[#4caf50] px-9 py-4 rounded-xl shadow-xl shadow-cyan-500/50 hover:shadow-cyan-400/70 transition-shadow duration-300">
+        Se connecter
+      </button>
+
+    </div>
   )
 }
