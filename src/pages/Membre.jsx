d9d456f0dd135cb4e9f87e5dbd1b6545c4367f57
Ikoriantsoa <rsfmamboara@gmail.com>
2024-12-03 19:37:43 +0300

    Création d'un navbar membre

--- src/pages/Membre.jsx
+++ src/pages/Membre.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Navbar from '../components/navbar/Navbar';
 
 export default function Membre() {
-  return (
-    <div className='bg-red-500'>Membre</div>
-  )
+    
+    return (
+      <div className="App">
+        <Navbar />
+        <main className="container mx-auto mt-10">
+          <h1 className="text-4xl font-bold text-center">Welcome to ShopEase</h1>
+          <p className="text-gray-600 text-center mt-4">
+            Discover amazing products at the best prices.
+          </p>
+        </main>
+      </div>
+    );
 }
