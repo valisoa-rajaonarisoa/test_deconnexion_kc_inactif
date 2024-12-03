94e36bc2c9f17edead17a587893e556fc52767e8
Ikoriantsoa <rsfmamboara@gmail.com>
2024-12-03 18:13:12 +0300

    Création des routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>
+  },
+  {
+    path: "/admin",
+    element: <h1>Admin</h1>
+  },
+  {
+    path: "/membre",
+    element: <h1>Membre</h1>
+  },
+  {
+    path: "/login",
+    element: <h1>Login</h1>
+  },
+  {
+    path: "/register",
+    element: <h1>Register</h1>
+  }
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={ route }></RouterProvider>
   </StrictMode>,
 )
